459bf913a028409c2069122ae9fdea874aa7279e
1119407848@qq.com <song1119407848@163.com>
2024-01-11 17:56:24 +0800

    本地提交

---
+++ status-machine/src/com/sjh/status/machine/base/BaseEvent.java
@@ -0,0 +1,7 @@
+package com.sjh.status.machine.base;
+
+/**
+ * 状态机事件接口
+ */
+public interface BaseEvent {
+}
---
+++ status-machine/src/com/sjh/status/machine/base/BaseStatus.java
@@ -0,0 +1,7 @@
+package com.sjh.status.machine.base;
+
+/**
+ * 状态机状态接口
+ */
+public interface BaseStatus {
+}
